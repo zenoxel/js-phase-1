b1675a05eb367450053da87ae8d2e69c6c16f1cd
zenoxel <nthengoc.dev@gmail.com>
2026-03-31 14:01:03 +0700

    Add CORS support and update package.json dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import {
   createUser,
   getAllUsers,
@@ -13,6 +14,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 // GET /users
